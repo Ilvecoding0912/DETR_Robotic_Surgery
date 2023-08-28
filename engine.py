4ff848161bca79a27d46af816f8c3b45fae5c6fd
Mobarakol Islam <mobarakol@u.nus.edu>
2023-08-28 19:08:22 +0100

    Update engine.py

--- engine.py
+++ engine.py
@@ -235,7 +235,7 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
         )
 
     val_freq = 0
-    for samples, targets in metric_logger.log_every(data_loader, 10, header):
+    for samples, targets in metric_logger.log_every(data_loader, 500, header):
         samples = samples.to(device)
         
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
